8277d2d55444839c361b6b7852e1274c3ae5596d
JI-Maina <irungujmaina@gmail.com>
2022-09-08 05:39:33 -0700

    Program to print smth with the use of function puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/* A program that exactly prints \"Programming is like building a multilingual puzzle using puts */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
