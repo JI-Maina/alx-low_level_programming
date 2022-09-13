f00d55c300ca3bc68d6e9d2803db9d9fb69e29ac
JI-Maina <irungujmaina@gmail.com>
2022-09-13 06:33:55 -0700

    Prints alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints lowercase alphabet x10 times
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int a = 0;
+
+	while (a <= 10)
+	{
+		_putchar(print_alphabet());
+	}
+}
