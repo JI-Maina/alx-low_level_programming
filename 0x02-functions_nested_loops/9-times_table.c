235ce5bf897e6416d6d2bcbf291d4ef7bd843308
JI-Maina <irungujmaina@gmail.com>
2022-09-14 05:43:10 -0700

    A function that prints 9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ *
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int i, j, product, ones, tens;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			product = (i * j);
+			tens = (product / 10);
+			ones = (product % 10);
+
+			if (j == 0)
+			{
+				_putchar(0 + '0');
+			}
+			else if (product < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(ones + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(tens + '0');
+				_putchar(ones + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
