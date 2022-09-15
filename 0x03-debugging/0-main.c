7ef154961a5dff3ddd0cd7d75ac3449d3f907ac9
JI-Maina <irungujmaina@gmail.com>
2022-09-14 20:55:40 -0700

    Debugging task 0 (positive_or_negative)

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - tests fuction that prints iff integer is positive or negative
+ * Retrun: 0 (success)
+ */
+
+void main(void)
+{
+	int i;
+
+	i = 0;
+	positive__or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+0x03-debugging tasks on debugging are found in this directory
