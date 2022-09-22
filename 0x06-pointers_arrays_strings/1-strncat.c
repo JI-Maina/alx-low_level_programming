7b87d763938595fa500f2e8917788abef88d9037
JI-Maina <irungujmaina@gmail.com>
2022-09-22 13:10:55 -0700

    A function that concatenates two strings to a given length

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ *
+ * @dest: string to be updated
+ * @src: string to be moved
+ * @n: number of characters to be updated
+ *
+ * Retrn: a pointer to the resulting string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	j = 0;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
