0c92a0d97c49efcc844e5a5cf9cf53149e0fd4be
JI-Maina <irungujmaina@gmail.com>
2022-09-28 13:59:57 -0700

    Inception. Is it possible?

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,3 +3,31 @@
 /**
  * _sqrt_recursion - finds natural sqrt of a number
  *
+ * @n: an integer to find its sqrt
+ *
+ * Return: the sqrt of n
+ */
+
+int _sqrt_recursion(int n)
+{
+	int i = 1;
+
+	if (n < -1)
+	{
+		return (-1);
+	}
+	else if (i * i == n)
+	{
+		return (i);
+	}
+
+
+	i++;
+
+	return (_sqrt_recursion(n));
+
+	if (i > n / 2)
+	{
+		return (-1);
+	}
+}
