bde45cce6045f0c56272730c29458df8450fd1b1
JI-Maina <irungujmaina@gmail.com>
2022-09-30 10:57:55 -0700

    A program that prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_func_name - prints the name of the program
+ * main - prints the name of the program
  *
  * @argc: arguement count
  * @argv: arguement vector
@@ -9,7 +9,7 @@
  * Return: void always
  */
 
-void print_func_name(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	int i;
 	(void)argc;
@@ -18,6 +18,9 @@ void print_func_name(int argc, char* argv[])
 	while (argv[0][i] != '\0')
 	{
 		_putchar(argv[0][i]);
+		i++;
 	}
 	_putchar('\n');
+
+	return(0);
 }
