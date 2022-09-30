6b7dfd932dc5292865cd7d76438ce650274c9c46
JI-Maina <irungujmaina@gmail.com>
2022-09-30 10:14:21 -0700

    A  program that prints the number of arguments passed into it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * args_num - prints the number of arguements passed into it
+ *
+ * @argc: arguement count
+ * @argv: arguement vector
+ *
+ * Return: int always
+ */
+
+int args_num(int argc, char* argv[])
+{
+	_putchar(argc);
+	_putchar('\n');
+}
