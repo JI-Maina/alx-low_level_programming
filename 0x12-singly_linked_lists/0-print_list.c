6ffc53414075e487a140391668d19d62329f1d74
JI-Maina <irungujmaina@gmail.com>
2022-10-24 09:04:25 -0700

    finally there

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,17 +11,17 @@
 size_t print_list(const list_t *h)
 {
 	size_t count;
-	const list_t *str = h;
+	const list_t *ptr = h;
 
 	count = 0;
-	while (h != NULL)
+	while (ptr != NULL)
 	{
-		if (str == NULL)
+		if (ptr->str == NULL)
 			printf("[0] (nill)\n");
 		else
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[%d] %s\n", ptr->len, ptr->str);
 		count++;
-		h = h->next;
+		ptr = ptr->next;
 	}
 
 	return (count);
