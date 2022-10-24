a8c74ad2a97ea0f2382a9e5d86201aef0a574ed3
JI-Maina <irungujmaina@gmail.com>
2022-10-24 09:13:09 -0700

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -19,7 +19,7 @@ size_t print_list(const list_t *h)
 		if (ptr->str == NULL)
 			printf("[0] (nill)\n");
 		else
-			printf("[%lu] %s\n", ptr->len, ptr->str);
+			printf("[%u] %s\n", ptr->len, ptr->str);
 		count++;
 		ptr = ptr->next;
 	}
