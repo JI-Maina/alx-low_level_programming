77b7a5851eb7cb4e93881af4b7531bbd21df28d1
JI-Maina <irungujmaina@gmail.com>
2022-10-25 01:28:22 -0700

    Real programmers can write assembly code in any language

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,22 @@
+;Declare needed C functions
+	extern  printf		; the c function to be called
+
+	section .data		; data section, initialized variables
+msg:	db "Hello, Holberton",0 ; c string needs 0
+fmt:	db "%s", 10, 0		; the printf format, "\n",'0'
+
+	section .text		; code section
+
+	global main		; the standard gcc entry point
+main:				; the program label for entry point
+	push	rbp		; set up stack frame, must be alligned
+
+	mov	rdi,fmt
+	mov	rsi,msg
+	mov	rax,0		; or can be  xor  rax,rax
+	call	printf		; Call C function
+
+	pop	rbp		; restore stack
+
+	mov	rax,0		; normal, no error, return value
+	ret			; return
