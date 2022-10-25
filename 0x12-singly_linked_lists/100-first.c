df794a726bdd31682d862f2a293de61efbaf5394
JI-Maina <irungujmaina@gmail.com>
2022-10-25 01:50:52 -0700

    The Hare and the Tortoise
    a function that prints before the main function is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * premain - function that runs before main
+ *
+ * Return: Nothing
+ */
+
+void __attribute__ ((constructor)) premain()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
