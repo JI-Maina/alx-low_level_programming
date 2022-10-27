88e897eb506cd4106197060a1afb0f992584c16b
JI-Maina <irungujmaina@gmail.com>
2022-10-27 05:56:21 -0700

    NULL check

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,17 +12,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	temp = *head;
-
 	if (head == NULL)
 		return;
 
-	while (temp != NULL)
+	while (*head != NULL)
 	{
-		temp = temp->next;
-		free(*head);
-		*head = temp;
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
 	}
-
-	head = NULL;
 }
