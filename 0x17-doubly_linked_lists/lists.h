ef4b3feded617a54a1c533888668752ef1b8f5ef
JI-Maina <irungujmaina@gmail.com>
2022-12-08 09:20:23 -0800

    Add node

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,6 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdlib.h>
 #include <stdio.h>
 
 /**
@@ -21,5 +22,6 @@ typedef struct dlistint_s
 
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 
 #endif /* LISTS_H */
