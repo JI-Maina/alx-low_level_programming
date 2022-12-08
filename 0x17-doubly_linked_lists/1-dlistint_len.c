f3ca16435b76752436fa7154671157f5610cd550
JI-Maina <irungujmaina@gmail.com>
2022-12-08 08:15:02 -0800

    List length

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns the number of elements in a linked dlis
+ *
+ * @h: doubly linked list
+ *
+ * Return: number of elements
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *temp = h;
+	size_t count = 0;
+
+	while (temp != NULL)
+	{
+		count++;
+		temp = temp->next;
+	}
+
+	return (count);
+}
